704daff9b4bdac7f713a2285137e0a1c1a706c31
MonicaLuca <monica.lucarini28@gmail.com>
2020-07-22 14:24:24 -0400

    countOnly.js added

---
+++ countOnly.js
@@ -0,0 +1,96 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+
+const eqArrays = function(array1, array2) {
+  let counter = 0;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        counter++;
+      }
+    }   
+    if (counter === array1.length) {
+      return true;  
+    } else {
+      return false;
+    } 
+  } else {
+    return false;
+  }
+};
+
+/*
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const countOnly = function(allItems, itemsToCount) {
+  const objCounter = {};
+  for (let item of allItems) {
+    if(objCounter[item] === undefined) {
+      objCounter[item] = 1
+    } else {
+      objCounter[item]++
+    }
+  }
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+console.log()
+/*
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+
+*/
+
+
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for(const item of allItems){
+    if(itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    } 
+  }
+    
+  return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
