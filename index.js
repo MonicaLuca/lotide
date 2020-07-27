464b80c9f6d8193b7179d443a5c3308e18141eba
MonicaLuca <monica.lucarini28@gmail.com>
2020-07-27 16:31:22 -0400

    index.js added

---
+++ index.js
@@ -0,0 +1,35 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require ('./countLetters')
+const countOnly = require('./countOnly')
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findkey = require('./findkey');
+const findKeyByValue = require('./findKeyByValue');
+const head = require('./head');
+const letterPositions = require ('./letterPositions')
+const map = require('./maps');
+const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil')
+const without = require('./without')
+
+module.exports = {
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays, 
+  eqObjects,
+  findkey,
+  findKeyByValue,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
+};
