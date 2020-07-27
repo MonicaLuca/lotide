f21fbc28ad1f0c4c126974ebebfdacff9ffd9eaf
MonicaLuca <monica.lucarini28@gmail.com>
2020-07-27 14:14:19 -0400

    middle.js export

--- middle.js
+++ middle.js
@@ -1,29 +1,3 @@
-const eqArrays = function(actual, expected) {
-  let counter = 0;
-  if (actual.length === expected.length) {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] === expected[i]) {
-        counter++;
-      }
-    }   
-    if (counter === actual.length) {
-      return true;  
-    } else {
-      return false;
-    } 
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} != ${expected}`);
-  }
-};
-
 const middle = function(arr) {
   let newArr = [];
   if (arr.length < 3) {
@@ -41,9 +15,4 @@ const middle = function(arr) {
   }
 };
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);  // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+module.exports = middle;
